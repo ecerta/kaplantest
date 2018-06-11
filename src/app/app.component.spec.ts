2067071a1924a437237d514b95a6a23d4d37fb6d
Liz Certa <eliz.certa@gmail.com>
2018-06-10 23:51:09 -0400

    complete Kaplan assessment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,20 +8,138 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render all of the items from dataArray on the page', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelectorAll('li')[0].textContent).toContain('Alpha');
+    expect(compiled.querySelectorAll('li')[3].textContent).toContain('Delta');
   }));
+
+  describe('reordering items', () => {
+    it('should reorder items correctly when reorderItems is called for down', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+
+      component.reorderItems(0, 'down');
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelectorAll('li')[0].textContent).toContain('Beta');
+      expect(compiled.querySelectorAll('li')[1].textContent).toContain('Alpha');
+    }));
+
+    it('should reorder items correctly when reorderItems is called for up', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+
+      component.reorderItems(2, 'up');
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelectorAll('li')[1].textContent).toContain('Gamma');
+      expect(compiled.querySelectorAll('li')[2].textContent).toContain('Beta');
+    }));
+
+    it('should reorder items appropriately when reset Items is called', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+
+      component.reorderItems(2, 'up');
+      fixture.detectChanges();
+      let compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelectorAll('li')[1].textContent).toContain('Gamma');
+      expect(compiled.querySelectorAll('li')[2].textContent).toContain('Beta');
+
+      component.resetItems();
+      fixture.detectChanges();
+      compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelectorAll('li')[1].textContent).toContain('Beta');
+      expect(compiled.querySelectorAll('li')[2].textContent).toContain('Gamma');
+    }));
+  });
+
+  describe('item selection', () => {
+
+    it('should have nothing selected by default', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelector('.active')).toBe(null);
+    }));
+
+    it('should apply the selected class to a selected option', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      compiled.querySelectorAll('li')[0].click();
+      fixture.detectChanges();
+
+      expect(compiled.querySelector('.active').textContent).toContain('Alpha');
+    }));
+
+    it('should alter the assistive text when an item is selected', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelector('.assistive-text').textContent).not.toContain('The active item is Alpha');
+
+      compiled.querySelectorAll('li')[0].click();
+      fixture.detectChanges();
+
+      expect(compiled.querySelector('.assistive-text').textContent).toContain('The active item is Alpha');
+    }));
+
+  });
+
+  describe('up and down buttons', () => {
+    it('should disable both up and down buttons by default', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelectorAll('button')[0].disabled).toBe(true);
+      expect(compiled.querySelectorAll('button')[1].disabled).toBe(true);
+    }));
+
+    it('should disable the up button when the first option is selected', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      compiled.querySelectorAll('li')[0].click();
+
+      fixture.detectChanges();
+
+      expect(compiled.querySelectorAll('button')[0].disabled).toBe(true);
+      expect(compiled.querySelectorAll('button')[1].disabled).toBe(false);
+    }));
+
+    it('should disable the down button when the last option is selected', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      compiled.querySelectorAll('li')[3].click();
+      fixture.detectChanges();
+
+      expect(compiled.querySelectorAll('button')[0].disabled).toBe(false);
+      expect(compiled.querySelectorAll('button')[1].disabled).toBe(true);
+    }));
+  });
+
 });
+
+// Given more time I'd do more reading into how to refactor some of this repetition out into a consistent beforeAll()
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public dataArray = [
+    'Alpha',
+    'Beta',
+    'Gamma',
+    'Delta'
+  ];
+  public selectedItemIndex = -1;
+  private originalArray = this.dataArray.slice(0);
+
+  public resetItems () {
+    this.selectedItemIndex = -1;
+    this.dataArray = this.originalArray;
+  }
+
+  // I thought about making reorderBasic the function that gets called from the HTML page
+  // But I thought just passing in the number could be confusing for a future dev looking at the HTML.
+  public reorderItems(index, direction) {
+    if (direction === 'up') {
+      this.reorderBasic(index, -1);
+    }
+    if (direction === 'down') {
+      this.reorderBasic(index, 1);
+    }
+  }
+
+  private reorderBasic(index, number) {
+    const temp = this.dataArray[index + number];
+    this.dataArray[index + number] = this.dataArray[index];
+    this.dataArray[index] = temp;
+    this.selectedItemIndex = index + number;
+  }
+
 }
